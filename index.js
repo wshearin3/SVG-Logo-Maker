4f11a14c18d9f9587d42bc0d433d0227872c5b37
Trey <trey.shearin3@gmail.com>
2024-03-13 09:32:10 -0400

    created first question prompting for initials, created validation function to check that user input is less than or equal to 3

--- index.js
+++ index.js
@@ -2,3 +2,20 @@
 
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+const validateInitials = input => {
+    if(input.length <= 3) {
+        return true;
+    } else {
+        return "Input must be three characters or less";
+    }
+};
+
+const questions = [
+    {
+        type: "input",
+        message: "Please enter up to three characters to be included in your logo.",
+        name: "initials",
+        validate: validateInitials
+    },
+]
